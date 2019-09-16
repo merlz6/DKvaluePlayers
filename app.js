d9a0904284578d4ef0bd75991a4128f7d409de09
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2019-09-16 15:15:59 -0400

    clean up

--- app.js
+++ app.js
@@ -9,32 +9,36 @@ fs.createReadStream('data.csv')
   .on('end', () => {
 
     for(let i = 0; i < results.length; i++){
-      //avg pts a game from string to number
-      if(results[i].AvgPointsPerGame[0] === '-'){
-        //do nothing there negative pts "FIlter negatives"
-      } else {
-      // if (results[i]['Game Info'].includes(teamsToAvoidPlaying) === false) {
 
-        let avgPtsNum = parseInt(results[i].AvgPointsPerGame)
-        // Number of points needed to "pay for salary" mlb = 500 a pt , nfl 363 a pt, soccer 600 a pt
-        //winning rounds usually around = soccer 100+ , mlb 120+, NFL 160+
-        let numberOfPtsPerDollar = 500
-        let salary = parseInt(results[i].Salary)
+      if(results[i].AvgPointsPerGame[0] === '-'){
+        //do nothing there negative pts - "FIlter negatives"
+      } else {
+        //avg pts a game from string to number
+      let avgPtsNum = parseInt(results[i].AvgPointsPerGame)
+      // Number of points needed to "pay for salary" mlb = 500 a pt , nfl 363 a pt, soccer 600 a pt
+      //winning rounds usually around = soccer 100+ , mlb 120+, NFL 160+
+      let numberOfPtsPerDollar = 500
+      let salary = parseInt(results[i].Salary)
+      // each players information
+      // console.log('=====================================')
+      // console.log('Name: ' + results[i].Name)
+      // console.log('Position: ' + results[i].Position);
+      // console.log('Salary: ' + results[i].Salary)
+      // console.log('Avg Points Per Game: ' + results[i].AvgPointsPerGame)
+      // console.log(results[i]['Game Info'])
+      // console.log('=====================================')
+
+
+
+
         //if playerpts per dollar is greater than game ptsPerDollar show player
         if(salary / avgPtsNum < numberOfPtsPerDollar){
-          // each players information
-          // console.log('=====================================')
-          // console.log('Name: ' + results[i].Name)
-          // console.log('Position: ' + results[i].Position);
-          // console.log('Salary: ' + results[i].Salary)
-          // console.log('Avg Points Per Game: ' + results[i].AvgPointsPerGame)
-          // console.log(results[i]['Game Info'])
-          // console.log('=====================================')
           valuePlayersBasedOnCost.push({
             'Name': results[i].Name,
             'Position': results[i].Position,
             'Salary' : parseInt(results[i].Salary),
             'AvgPointsPerGame' : parseInt(results[i].AvgPointsPerGame),
+            'team':results[i].TeamAbbrev,
             'GameInfo': results[i]['Game Info'],
             'AvgPtsPerGameOverSalaryPtsExpected': (parseInt(results[i].AvgPointsPerGame) - (parseInt(results[i].Salary)/numberOfPtsPerDollar))
         })
@@ -49,7 +53,7 @@ fs.createReadStream('data.csv')
         console.log(sortedList)
       }
 
-
+      console.log(results[1])
     }
 
 
